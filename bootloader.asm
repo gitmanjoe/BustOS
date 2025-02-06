39d0d70b7bf0422174098bae138945e1b5d718e3
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-06 10:50:20 -0500

    Prints '?'

--- bootloader.asm
+++ bootloader.asm
@@ -5,11 +5,11 @@ bits 16
 ; lood bootloader at this address 0x7c00
 ; hence offsets should be calculated in relation to this address
 org 0x7c00
+
 mov al,'?'
 call print_char
+
 jmp $ ; loop forever
-times 510 - ($-$$) db 0
-dw 0xaa55
 
 print_char:
     mov ah,0x0e ; BIOS teletype output
@@ -17,3 +17,7 @@ print_char:
     int 0x10 ; print the character in al
     pop bp
     ret
+
+times 510 - ($-$$) db 0
+dw 0xaa55
+
