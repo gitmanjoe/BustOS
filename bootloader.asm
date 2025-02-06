b2d6a82a102aa0672716b5dd66bfa50c420b964c
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-06 10:44:15 -0500

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -5,6 +5,15 @@ bits 16
 ; lood bootloader at this address 0x7c00
 ; hence offsets should be calculated in relation to this address
 org 0x7c00
+mov al,'?'
+call print_char
 jmp $ ; loop forever
 times 510 - ($-$$) db 0
 dw 0xaa55
+
+print_char:
+    mov ah,0x0e ; BIOS teletype output
+    push bp
+    int 0x10 ; print the character in al
+    pop bp
+    ret
