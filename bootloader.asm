65c13d4604acca53ceeac5d16cc0722e9993e949
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-06 17:22:10 -0500

    is broken

--- bootloader.asm
+++ bootloader.asm
@@ -2,17 +2,16 @@
 bits 16
 org 0x7c00
 
-mov bx, Welcome_Msg
-call print_string
 mov bx, Real_Msg
 call print_string
+mov ax, 0x0000
+mov ds, ax
+mov cx, 100
 jmp $
 
 ; Data
-Welcome_Msg:
-db 'BustOS is Starting Up',13,10,0 
 Real_Msg:
-db 'BustOS is in 16 bits Real Mode',13,10,0 
+db 'BustOS is in 16 bit Real Mode',13,10,0 
 
 print_char:
     mov ah,0x0e ; BIOS teletype output
@@ -69,30 +68,13 @@ print_hex_loop:
     ret
 ; GDT
 gdt_start :
-gdt_null : ; the mandatory null descriptor
-dd 0x0 ; 'dd' means define double word ( i.e. 4 bytes )
-dd 0x0
-gdt_code : ; the code segment descriptor
-dw 0xffff ; Limit ( bits 0 -15)
-dw 0x0 ; Base ( bits 0 -15)
-db 0x0 ; Base ( bits 16 -23)
-db 10011010b ; 1st flags , type flags
-db 11001111b ; 2nd flags , Limit ( bits 16 -19)
-db 0x0 ; Base ( bits 24 -31)
-gdt_data : ; the data segment descriptor
-dw 0xffff ; Limit ( bits 0 -15)
-dw 0x0 ; Base ( bits 0 -15)
-db 0x0 ; Base ( bits 16 -23)
-db 10010010b ; 1st flags , type flags
-db 11001111b ; 2nd flags , Limit ( bits 16 -19)
-db 0x0 ; Base ( bits 24 -31)
+    dq 0x0000000000000000
+    dq 0x00CF9A000000FFFF
+    dq 0x00CF92000000FFFF
 gdt_end :
 gdt_descriptor :
-dw gdt_end - gdt_start - 1 ; Size of our GDT , always less one
-; of the true size
-dd gdt_start ; Start address of our GDT
-CODE_SEG equ gdt_code - gdt_start
-DATA_SEG equ gdt_data - gdt_start
+    dw gdt_end - gdt_start - 1 ; Size of our GDT 
+    dd gdt_start ; Start address of our GDT
 
 times 510 - ($-$$) db 0
 dw 0xaa55
