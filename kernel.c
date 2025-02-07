6859a74c27cb1c4e04f889b5f467768a00d50a2d
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-07 11:41:41 -0500

    little migger

--- kernel.c
+++ kernel.c
@@ -3,10 +3,9 @@
 #include "screen.h"
 
 void kernel() {
-    clear_screen(TEXT_COLOUR);
-    char sigma[] = "Hello, World";
-    char sigma1[] = "die, sigma";
-    //printf(sigma, TEXT_COLOUR);
+    //clear_screen(TEXT_COLOUR);
+    char sigma[] = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    printf(sigma, TEXT_COLOUR);
     //printf(sigma1, TEXT_COLOUR);
     while (true) {}
 }
--- screen.c
+++ screen.c
@@ -4,6 +4,8 @@
 #define VIDEO_ADDRESS 0xb8000
 #define NEW_LINE_HEX 0xA0
 
+int offset[] = {0,0};
+
 void printChar(char charToPrint, int color, char* vmp)
 {
     //*vmp++ = charToPrint;
@@ -13,6 +15,7 @@ void printChar(char charToPrint, int color, char* vmp)
 void printf(char strtoPrint[], int color)
 {
     char* vmp = (char*) VIDEO_ADDRESS;
+    vmp += NEW_LINE_HEX;
     for(int i = 0; strtoPrint[i] != 0; i++){
         *vmp++ = strtoPrint[i];
         *vmp++ = color;
@@ -22,12 +25,13 @@ void printf(char strtoPrint[], int color)
 
 void crlf()
 {
-    
+    offset[0] = 0;
+    offset[1] = offset[1] + 0xA0;
 }
 
 void clear_screen(int color){
     char* vmp = (char*) VIDEO_ADDRESS;
-    vmp += 0xA0;
+    vmp += NEW_LINE_HEX;
     for(int i = 0; i <= 80 * 25 * 2; i++){
         *vmp++ = ' ';
         *vmp++ = color;
