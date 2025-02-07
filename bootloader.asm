991f9d5a03c035af0a0b380f461df4aba3652bfa
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-06 23:22:56 -0500

    C code is functional

--- bootloader.asm
+++ bootloader.asm
@@ -83,29 +83,5 @@ dw 0xaa55
 
 [ bits 32]
 BEGIN_PM :
-mov ebx , MSG_PROT_MODE
-call print_string_pm ; print our message using 32 bit print string
-jmp $
-
-[bits 32]
-; Define video constants
-VIDEO_MEMORY equ 0xb8000
-WHITE_ON_BLACK equ 0x0f
-print_string_pm :
-    pusha
-    mov edx , VIDEO_MEMORY ; Set edx to the start of vid mem.
-print_string_pm_loop :
-    mov al , [ ebx ] ; Store the char at EBX in AL
-    mov ah , WHITE_ON_BLACK ; Store the attributes in AH
-    cmp al , 0 ; if (al == 0) , at end of string , so
-    je done ; jump to done
-    mov [edx] , ax ; Store char and attributes at current
-    ; character cell.
-    add ebx , 1 ; Increment EBX to the next char in string.
-    add edx , 2 ; Move to next character cell in vid mem.
-    jmp print_string_pm_loop ; loop around to print the next char.
-print_string_pm_done :
-    popa
-    ret ; Return from the function
 
 times 2048 -( $ - $$ ) db 0
--- kernel.c
+++ kernel.c
@@ -1,4 +1,7 @@
-void main() {
+# define TEXT_COLOUR 0x0f
+
+void kernel() {
     char* video_memory = (char*) 0xb8000;
-    *video_memory = 'X';
+    *video_memory++ = 'X';
+    *video_memory++ = TEXT_COLOUR;
 }
