b8e352c9cbd3424aea7d8b3151550ed4157dd979
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-07 09:15:41 -0500

    a

--- screen.h
+++ screen.h
@@ -1,4 +1,6 @@
-static void printChar (char charToPrint)
+static void printChar (char charToPrint int colo)
 {
-    
+    char* video_memory = (char*) 0xb8000;
+    *video_memory++ = charToPrint;
+    *video_memory++ = TEXT_COLOUR;
 }
