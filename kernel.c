fa56121874b567631ad6a85fb8b466f7792d5831
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-07 09:22:59 -0500

    a

--- kernel.c
+++ kernel.c
@@ -1,9 +1,12 @@
 #define TEXT_COLOUR 0xf0
 #include <stdbool.h>
+#include <screen.h>
 
 void kernel() {
-    char* video_memory = (char*) 0xb8000;
+    char letter = 'B'
+    /*char* video_memory = (char*) 0xb8000;
     *video_memory++ = 'X';
-    *video_memory++ = TEXT_COLOUR;
+    *video_memory++ = TEXT_COLOUR;*/
+    printChar (letter, TEXT_COLOUR);
     while (true) {}
 }
