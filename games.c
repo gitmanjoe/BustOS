918feab694c183fb3b78bf7adff7250ade2f1f6b
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-07 13:30:30 -0500

    Added Tuxsay

--- games.c
+++ games.c
@@ -52,3 +52,63 @@ void cowsay(char strToCow[], int color)
     printf(cow4, color);
     printf(cow5, color);
 }
+
+void tuxsay(char strToSay[], int color)
+{
+    //Get the length of the string
+    int strLen = 0;
+    for(int i = 0; strToSay[i] != 0; i++)
+    {
+        strLen++;
+    }
+
+    //Get the line of _'s on top
+    char udsc[32];
+    for(int i = strLen - 1; i >= 0; i--)
+    {
+        udsc[i] = '_';
+    }
+
+    //Get the line of -'s underneath
+    char dash[32];
+    for(int i = strLen - 1; i >= 0; i--)
+    {
+        dash[i] = '-';
+    }
+
+    //Variable initalization
+    char twoSpaces[] = "  ";
+    char newLine[] = "\n";
+    char lessThanSpace[] = "< ";
+    char greaterThanSpace[] = " >";
+    char tux1[] = "   \\\n";
+    char tux2[] = "    \\\n";
+    char tux3[] = "        .--.\n";
+    char tux4[] = "       |o_o |\n";
+    char tux5[] = "       |:_/ |\n";
+    char tux6[] = "      //   \\ \\\n";
+    char tux7[] = "     (|     | )\n";
+    char tux8[] = "    /'\\_   _/`\\\n";
+    char tux9[] = "    \\___)=(___/\n";
+
+    //PRINT TUX!!!!!
+    printf(twoSpaces, color);
+    printf(udsc, color);
+    printf(newLine, color);
+    printf(lessThanSpace, color);
+    printf(strToSay, color);
+    printf(greaterThanSpace, color);
+    printf(newLine, color);
+    printf(twoSpaces, color);
+    printf(dash, color);
+    printf(newLine, color);
+    printf(tux1, color);
+    printf(tux2, color);
+    printf(tux3, color);
+    printf(tux4, color);
+    printf(tux5, color);
+    printf(tux6, color);
+    printf(tux7, color);
+    printf(tux8, color);
+    printf(tux9, color);
+}
