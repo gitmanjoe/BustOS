27c88773df2d8fddcc3f785901e8f018ccfc5617
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-06 23:27:35 -0500

    Sorry linux users 👎

---
+++ run.bat
@@ -0,0 +1,6 @@
+nasm -f bin bootloader.asm -o bootloader.bin
+gcc -m32 -ffreestanding -c kernel.c -o kernel.o
+ld -T NUL -mi386pe -o kernel.tmp -Ttext 0x7e00 kernel.o
+objcopy -O binary -j .text kernel.tmp kernel.bin
+copy /b bootloader.bin+kernel.bin+padding.bin BustOS.img
+"C:\Program Files\qemu\qemu-system-x86_64.exe" BustOS.img
