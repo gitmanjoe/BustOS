172346e99c664b036e0e333100f023a14cf1fbbb
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-07 09:23:53 -0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,6 +1,7 @@
 nasm -f bin bootloader.asm -o bootloader.bin
 gcc -m32 -ffreestanding -c kernel.c -o kernel.o
-ld -T NUL -mi386pe -o kernel.tmp -Ttext 0x7e00 kernel.o
+gcc -m32 -ffreestanding -c screen.c -o screen.o
+ld -T NUL -mi386pe -o kernel.tmp -Ttext 0x7e00 kernel.o screen.o
 objcopy -O binary -j .text kernel.tmp kernel.bin
 copy /b bootloader.bin+kernel.bin+padding.bin BustOS.img
 "C:\Program Files\qemu\qemu-system-x86_64.exe" BustOS.img
