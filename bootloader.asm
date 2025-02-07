c760f0b1fc217211146ed16c2432772c40559e59
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-06 20:39:41 -0500

    Minor Bug Fixes

--- bootloader.asm
+++ bootloader.asm
@@ -4,12 +4,13 @@ org 0x7c00
 
 mov bx, Real_Msg
 call print_string
+mov [ BOOT_DRIVE ], dl ; BIOS stores our boot drive in dl
 mov ah, 0x02           ; BIOS read sectors function
-mov al, 20              ; Number of sectors to read
+mov al, 128            ; Number of sectors to read
 mov ch, 0              ; Cylinder 0
 mov cl, 2              ; Start reading at sector 2 (after the boot sector)
 mov dh, 0              ; Head 0
-mov dl, 0x80           ; Drive number (0 for floppy, 0x80 for hard disk)
+mov dl, [ BOOT_DRIVE ] ; Drive number (0 for floppy, 0x80 for hard disk)
 mov bx, 0x7e00         ; Load additional sectors at 0x8000
 int 0x13               ; BIOS interrupt to read sectors
 lgdt [gdt_descriptor] 
@@ -20,10 +21,11 @@ cli
 jmp 0x08:init_pm ; Selector 0x08 points to the code segment
 
 ; Data
+BOOT_DRIVE : db 0
 Real_Msg:
 db 'BustOS is in 16 bit Real Mode',13,10,0 
 MSG_PROT_MODE:
-db "32 - bit Protected Mode " , 0
+db "32-bit Protected Mode Initialized", 0
 
 print_char:
     mov ah,0x0e ; BIOS teletype output
