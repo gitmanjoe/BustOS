62cbb3c4f7d69fd69121b184963a9f841ae25d85
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-07 13:04:16 -0500

    Put messages into variables so it worked

--- games.c
+++ games.c
@@ -3,34 +3,52 @@
 
 void cowsay(char strToCow[], int color)
 {
+    //Getting the length of the string
     int strLen = 0;
     for(int i = 0; strToCow[i] = 0; i++)
     {
         strLen++;
     }
 
+    //Getting the line of _'s on top
     char udsc[32];
     for(int i = strLen - 1; i >= 0; i--)
     {
         udsc[i] = '_';
     }
 
+    //Getting the line of -'s underneath
     char dash[32];
     for(int i = strLen - 1; i >= 0; i--)
     {
         dash[i] = '-';
     }
 
-    printf("  ", color);
+    //Variable initalization
+    char twoSpaces[] = "  ";
+    char newLine[] = "\n"
+    char lessThanSpace[] = "< ";
+    char greaterThanSpace[] = "> ";
+    char cow1[] = "         \\   ^__^ \n";
+    char cow2[] = "          \\  (oo)\\_______\n";
+    char cow3[] = "             (__)\\       )\\/\\\\";
+    char cow4[] = "                 ||----w |\n";
+    char cow5[] = "                 ||     ||\n";
+
+    //PRINT THE COW!!!!!
+    printf(twoSpaces, color);
     printf(udsc, color);
-    printf("\n", color);
-    printf("< ", color);
+    printf(newLine, color);
+    printf(lessThanSpace, color);
     printf(strToCow, color);
-    printf("  ", color);
+    printf(greaterThanSpace, color);
+    printf(newLine, color);
+    printf(twoSpaces, color);
     printf(dash, color);
-    printf("         \\   ^__^ \n", color);
-    printf("          \\  (oo)\\_______\n", color);
-    printf("             (__)\\       )\\/\\\\", color);
-    printf("                 ||----w |\n", color);
-    printf("                 ||     ||\n", color);
+    printf(newLine, color);
+    printf(cow1, color);
+    printf(cow2, color);
+    printf(cow3, color);
+    printf(cow4, color);
+    printf(cow5, color);
 }
