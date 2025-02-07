c1a0e99e9210c6803f9fad314cf000c41211c0f4
JxTech <91810057+gitmanjoe@users.noreply.github.com>
2025-02-07 09:12:38 -0500

    fixed code loop

--- kernel.c
+++ kernel.c
@@ -1,7 +1,9 @@
-# define TEXT_COLOUR 0x0f
+#define TEXT_COLOUR 0xf0
+#include <stdbool.h>
 
 void kernel() {
     char* video_memory = (char*) 0xb8000;
     *video_memory++ = 'X';
     *video_memory++ = TEXT_COLOUR;
+    while (true) {}
 }
