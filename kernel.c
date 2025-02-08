d885115ce16dc5e02c9e47ea3b145be430ce0ed4
Link-Games <104279962+Link-Games@users.noreply.github.com>
2025-02-07 21:40:15 -0500

    Fixed Typos

--- kernel.c
+++ kernel.c
@@ -8,7 +8,7 @@ void kernel() {
     clear_screen(0x0f);
     char welcome[] = "Welcome To BustOS - 32 Bit Protected Mode\n";
     char moo[] = "Moo";
-    char tux[] = "bustos is better than linux";
+    char tux[] = "BustOS is better than Linux";
     printf(welcome, TEXT_COLOUR);
     cowsay(moo, TEXT_COLOUR);
     tuxsay(tux, TEXT_COLOUR);
