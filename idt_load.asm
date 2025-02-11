1bcc3fa95f092d56a52645238a37e3ec63704ea1
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-10 20:00:44 -0800

    BYE BYE YOU ARE DELETED!!!!

--- idt_load.asm
+++
@@ -1,7 +0,0 @@
-[bits 32]
-global load_idt    ; Export load_idt for linking
-
-load_idt:
-    mov eax, [esp+4]  ; Load the pointer from stack
-    lidt [eax]        ; Load the IDT
-    ret
--- idt_pic.h
+++
@@ -1,8 +0,0 @@
-#ifndef IDT_PIC_H
-#define IDT_PIC_H
-
-#include <stdint.h>
-
-void setup_idt_and_pic();
-
-#endif
