d9d4f941b2c0d8b987cefa52b680ecfb8bcdc6ef
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-10 20:06:18 -0800

    Update tools.c

--- tools.c
+++ tools.c
@@ -3,7 +3,7 @@
 
 #define BLK_WHI 0x0f
 
-void print_logo(){char logo1[] = " ____            _    ___  ____ \n";char logo2[] = "| __ ) _   _ ___| |_ / _ \\/ ___|\n";char logo3[] = "|  _ \\| | | / __| __| | | \\___ \\ \n";char logo4[] = "| |_) | |_| \\__ \\ |_| |_| |___) |\n";char logo5[] = "|____/ \\__,_|___/\\__|\\___/|____/ \n";printf(logo1, BLK_WHI);printf(logo2, BLK_WHI);printf(logo3, BLK_WHI);printf(logo4, BLK_WHI);printf(logo5, BLK_WHI);}
+void print_logo(int color){char logo1[] = " ____            _    ___  ____ \n";char logo2[] = "| __ ) _   _ ___| |_ / _ \\/ ___|\n";char logo3[] = "|  _ \\| | | / __| __| | | \\___ \\ \n";char logo4[] = "| |_) | |_| \\__ \\ |_| |_| |___) |\n";char logo5[] = "|____/ \\__,_|___/\\__|\\___/|____/ \n";printf(logo1, color);printf(logo2, color);printf(logo3, color);printf(logo4, color);printf(logo5, color);}
 void enable_interrupts(){__asm__ volatile("sti");}
 int str_to_int(char* str){
     int num = 0;
