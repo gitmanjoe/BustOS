5a0700f1272b40f40975c03ef780505864c8fb1b
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-02-10 20:07:27 -0800

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -10,7 +10,7 @@ void kernel()
     clear_screen(TEXT_COLOUR);
     print_logo(TEXT_COLOUR);
     char welcome[] = "Welcome To BustOS - 32 Bit Protected Mode\n";
-    char moo[] = "Moo";
+    char moo[] = "I love BBC";
     char tux[] = "BustOS is better than Linux";
     printf(welcome, TEXT_COLOUR);
     cowsay(moo, TEXT_COLOUR);
