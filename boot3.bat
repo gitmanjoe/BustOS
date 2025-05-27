f66c5dffb3399bf7041bbc51f870e9b93211014a
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-05-27 10:37:55 -0400

    fix

--- boot3.bat
+++ boot3.bat
@@ -4,13 +4,14 @@ gcc -m32 -ffreestanding -c kernel.c -o kernel.o
 rem objdump -d kernel.o
 gcc -m32 -ffreestanding -c ports.c -o ports.o
 gcc -m32 -ffreestanding -c mem.c -o mem.o
+gcc -m32 -ffreestanding -c cursor.c -o cursor.o
 gcc -m32 -ffreestanding -c screen.c -o screen.o
 gcc -m32 -ffreestanding -c strings.c -o strings.o
 gcc -m32 -ffreestanding -c keyboard.c -o keyboard.o
 gcc -m32 -ffreestanding -c isr.c -o isr.o
 gcc -m32 -ffreestanding -c idt.c -o idt.o
 "C:\Program Files\nasm\nasm" -f elf interrupts.asm -o interrupts.o
-ld -T NUL -m i386pe -o kernel.tmp -Ttext 0x7e00 kernel.o ports.o mem.o screen.o strings.o keyboard.o isr.o idt.o interrupts.o
+ld -T NUL -m i386pe -o kernel.tmp -Ttext 0x7e00 kernel.o ports.o mem.o cursor.o screen.o strings.o keyboard.o isr.o idt.o interrupts.o
 objcopy -O binary -j .text kernel.tmp kernel.bin
 rem "C:\Program Files\nasm\ndisasm" -b 32 kernel.bin
 copy /b bootloader.bin+kernel.bin+padding.bin BustOS.img
