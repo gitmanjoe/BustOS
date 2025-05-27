6f62d6f5f1736a87fcd3b8131ed490c53937e699
JxTech <xavierwendt@gmail.com>
2025-05-27 10:29:20 -0400

    Update boot3.bat

--- boot3.bat
+++ boot3.bat
@@ -10,8 +10,7 @@ gcc -m32 -ffreestanding -c keyboard.c -o keyboard.o
 gcc -m32 -ffreestanding -c isr.c -o isr.o
 gcc -m32 -ffreestanding -c idt.c -o idt.o
 "C:\Program Files\nasm\nasm" -f elf interrupts.asm -o interrupts.o
-ld -T NUL -m i386pe -o kernel.tmp -Ttext 0x7e00 kernel.o ports.o mem.o screen.o strings.o 
-keyboard.o isr.o idt.o interrupts.o
+ld -T NUL -m i386pe -o kernel.tmp -Ttext 0x7e00 kernel.o ports.o mem.o screen.o strings.o keyboard.o isr.o idt.o interrupts.o
 objcopy -O binary -j .text kernel.tmp kernel.bin
 rem "C:\Program Files\nasm\ndisasm" -b 32 kernel.bin
 copy /b bootloader.bin+kernel.bin+padding.bin BustOS.img
