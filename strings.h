83285364dae4486aff6259de7994448588ecb25e
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-05-27 11:22:54 -0400

    fixed

--- strings.h
+++ strings.h
@@ -1,8 +1,5 @@
 // strings.h
 #pragma once
-// string manipulation functions
-#define low_16(address) (unsigned short)((address) & 0xFFFF)
-#define high_16(address) (unsigned short)(((address) >> 16) & 0xFFFF)
 int string_length(char s[]);
 void reverse(char s[]);
 void int_to_string(int n, char str[]);
