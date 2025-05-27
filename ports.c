bfaa20d67bf034fb28b7e1cb966f7e196e62ab45
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-05-27 10:26:41 -0400

    aaa

--- ports.c
+++ ports.c
@@ -14,7 +14,7 @@ unsigned char port_byte_in(unsigned short port) {
 void port_byte_out(unsigned short port, unsigned char data) {
     __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
 }
-
+//a
 unsigned short port_word_in(unsigned short port) {
  unsigned short result;
     __asm__("in %%dx, %%ax" : "=a" (result) : "d" (port));
