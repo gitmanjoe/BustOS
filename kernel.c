765cd6ea47b40cc65750545a9a0f58926b0831d2
DOOMZOOM <104279962+Link-Games@users.noreply.github.com>
2025-05-27 10:48:31 -0400

    revert to old kernel

--- kernel.c
+++ kernel.c
@@ -1,40 +1,21 @@
-// kernel3.c
-// install the ISRs, loading IDT.
-// enable external interrupts by setting the interrupt flag using sti.
-// call the init_keyboard function that registers the keyboard interrupt handler.
+#define TEXT_COLOUR 0x0a
+#include <stdbool.h>
+#include "games.h"
 #include "screen.h"
-#include "strings.h"
-#include "isr.h"
-#include "keyboard.h"
-void kernel () {
+#include "cursor.h"
+#include "tools.h"
+
+void kernel() 
+{
+    set_colour(TEXT_COLOUR);
     clear_screen();
-    char message[] = "BustOS Kernel running in 32 bit protected mode";
-    printf(message);
-    char msg1[] = "Installing interrupt service routines (ISRs).\n";
-    printf(msg1);
-    isr_install();
-    char msg2[] = "Enabling external interrupts.\n";
-    printf(msg2);
-    asm volatile("sti");
-    char msg3[] = "Initializing keyboard (IRQ 1).\n";
-    printf(msg3);
-    init_keyboard();
-    char prompt[] = "\nBustOS> ";
-    printf(prompt);
-    while(1==1){}
-}
-// receive commands to execute
-void execute_command(char* input) {
- // exit command
- char exitmsg[] = "EXIT";
-if (compare_string(input, exitmsg) == 0) {
-char msg[] = "Exit\n";
-printf(msg);
-asm volatile("hlt"); // halt cpu
-}
-char msg[] = "\ncommand: ";
-printf(msg);
-printf(input);
-char prompt[] = "\nBustOS> ";
-printf(prompt);
+    print_logo();
+    char welcome[] = "Welcome To BustOS - 32 Bit Protected Mode\n";
+    char moo[] = "I love BBC";
+    char tux[] = "Linux Sucks";
+    printf(welcome);
+    //cowsay(moo);
+    //tuxsay(tux);
+    set_cursor(69, 69);
+    while (true) {}
 }
