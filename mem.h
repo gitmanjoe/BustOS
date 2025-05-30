925ac3dd7d881e7eec90980d8a0916da83860ed4
Link-Games <benjamin.zdunich@outlook.com>
2025-05-30 15:19:28 -0400

    Already existed in tools.c

--- mem.h
+++
@@ -1,7 +0,0 @@
-// mem.h
-#pragma once
-// memory copy and set value functions
-void memory_copy(unsigned char* source, unsigned char* destination, 
-int bytes);
-void memory_set(unsigned char* destination, unsigned char value, 
-unsigned int len);
