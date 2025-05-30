61ef20dd40e6b1d9896c220fbaa5c07a2acae6c1
Link-Games <benjamin.zdunich@outlook.com>
2025-05-30 15:19:38 -0400

    added tools.c

--- boot3.bat
+++ boot3.bat
@@ -3,7 +3,6 @@ rem boot3.bat
 gcc -m32 -ffreestanding -c kernel.c -o kernel.o
 rem objdump -d kernel.o
 gcc -m32 -ffreestanding -c ports.c -o ports.o
-gcc -m32 -ffreestanding -c mem.c -o mem.o
 gcc -m32 -ffreestanding -c cursor.c -o cursor.o
 gcc -m32 -ffreestanding -c screen.c -o screen.o
 gcc -m32 -ffreestanding -c strings.c -o strings.o
@@ -11,6 +10,7 @@ gcc -m32 -ffreestanding -c keyboard.c -o keyboard.o
 gcc -m32 -ffreestanding -c isr.c -o isr.o
 gcc -m32 -ffreestanding -c idt.c -o idt.o
 gcc -m32 -ffreestanding -c hardware.c -o hardware.o
+gcc -m32 -ffreestanding -c tools.c -o tools.o
 "C:\Program Files\nasm\nasm" -f elf interrupts.asm -o interrupts.o
 ld -T NUL -m i386pe -o kernel.tmp -Ttext 0x7e00 kernel.o ports.o mem.o cursor.o screen.o strings.o keyboard.o isr.o idt.o interrupts.o
 objcopy -O binary -j .text kernel.tmp kernel.bin
