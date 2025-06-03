553531712594c6bbda51b3f5fdd6337464b5e50e
Link-Games <benjamin.zdunich@outlook.com>
2025-06-03 12:52:23 -0400

    added sleep_ms()

--- tools.h
+++ tools.h
@@ -8,3 +8,5 @@ int str_to_int(char* str);
 void memory_copy(char *source, char *dest, int nbytes);
 void memory_set(u8 *dest, u8 val, u32 len);
 void int_to_ascii(int n, char str[]);
+
+void sleep_ms(unsigned int ms);
